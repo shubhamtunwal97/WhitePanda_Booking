c244603d4d5da8510c4d901581ef5e966c704d61
shubhamtunwal97 <shubhamtunwal97@gmail.com>
2019-10-13 15:16:22 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 let mongoose = require('mongoose');
 
-const server = 'mongodb+srv://admin:admin123@cluster0-dwjis.mongodb.net/test?retryWrites=true&w=majority'; // REPLACE WITH YOUR DB SERVER
+//const server = 'mongodb+srv://admin:admin123@cluster0-dwjis.mongodb.net/test?retryWrites=true&w=majority'; // REPLACE WITH YOUR DB SERVER
 const database = 'test';      // REPLACE WITH YOUR DB NAME
+const server = 'mongodb+srv://admin:admin123@cluster0-dwjis.mongodb.net'; // REPLACE WITH YOUR DB SERVER
 
 //this.url = 'mongodb://pawan64:feelsgood64@ds263590.mlab.com:63590/user64
 module.exports  = class Database {
