2f972c646f045ca417499870a98e11b649154c42
shubhamtunwal97 <shubhamtunwal97@gmail.com>
2019-10-13 15:30:08 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,23 +1,22 @@
 let mongoose = require('mongoose');
-
-//const server = 'mongodb+srv://admin:admin123@cluster0-dwjis.mongodb.net/test?retryWrites=true&w=majority'; // REPLACE WITH YOUR DB SERVER
-const database = 'test';      // REPLACE WITH YOUR DB NAME
+const database = 'test';    
 const server = 'mongodb+srv://admin:admin123@cluster0-dwjis.mongodb.net'; // REPLACE WITH YOUR DB SERVER
 
-//this.url = 'mongodb://pawan64:feelsgood64@ds263590.mlab.com:63590/user64
 module.exports  = class Database {
   constructor() {
     this._connect()
   }
   
 _connect() {
-     mongoose.connect(`mongodb://${server}/${database}`)
+     mongoose.connect(`${server}/${database}`)
        .then(() => {
          console.log('Database connection successful')
        })
        .catch(err => {
-         console.error('Database connection error')
+         console.error(err)
        })
   }
 }
+
+
 
